d9b507ab95b1f2b837664f5ac75e70c349099fd9
christyanvittori <christyanvittori@gmail.com>
2025-03-19 07:23:24 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,13 @@
+
+<?php
+$num1 = rand(1, 10);
+$num2 = rand(1, 10);
+$result = $num1 + $num2;
+echo "What is $num1 + $num2?";
+$answer = trim(fgets(STDIN));
+if ($answer == $result) {
+    echo "Correct!";
+} else {
+    echo "Incorrect, the answer is $result.";
+}
+?>
