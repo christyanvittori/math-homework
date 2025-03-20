0b9022cb5dc313b3351e222c90fbe9350262004b
christyanvittori <christyanvittori@gmail.com>
2025-03-20 10:15:15 +0000

    Add 14.php

---
+++ src/14.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Math Homework</title>
+    </head>
+    <body>
+        <?php
+            $num1 = rand(1, 5); // random number between 1 and 5
+            $num2 = rand(6, 10); // random number between 6 and 10
+            
+            echo "What is $num1 + $num2?";
+        ?>
+    </body>
+</html>
