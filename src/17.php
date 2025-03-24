434a6fee0f7ed3f154275db7543c58764a5ae00c
christyanvittori <christyanvittori@gmail.com>
2025-03-24 13:28:03 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,7 @@
+<?php
+$students = ["Student1", "Student2", "Student3"];
+$count = count($students);
+for ($i = 0; $i < $count; $i++) {
+    echo "Student{$i} has a score of {$studentScore[$i]}";
+}
+?>
