9e52a60f6f1933b41c11d0e0e1491d56bb0f3666
christyanvittori <christyanvittori@gmail.com>
2025-04-01 16:10:25 +0000

    Add 21.php

---
+++ src/21.php
@@ -0,0 +1,9 @@
+<?php
+// This is an example of basic arithmetic operations.
+// It calculates 5 + 3 and prints the result on the screen.
+
+$result = 5 + 3;
+echo $result;
+
+// You can add more complex calculations here if needed.
+?>
