ddd77332448e253a8c613697918cca6fe88a4960
christyanvittori <christyanvittori@gmail.com>
2025-04-03 16:30:14 +0000

    Add 23.php

---
+++ src/23.php
@@ -0,0 +1,11 @@
+<?php
+// Example PHP code for generating random data
+$randomData = array(
+    "name" => "John Doe",
+    "age" => 30,
+    "job" => "Engineer",
+    "country" => "USA"
+);
+
+print_r($randomData);
+?>
