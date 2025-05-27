a57aac416bf8a0ed58346ea7f2426cfdb9b18da1
christyanvittori <christyanvittori@gmail.com>
2025-05-27 12:21:27 +0000

    Add 43.php

---
+++ src/43.php
@@ -0,0 +1,8 @@
+<?php
+// This is an example of using arrays to store student information.
+$students = [
+    ["name" => "Alice", "age" => 20],
+    ["name" => "Bob", "age" => 18],
+    ["name" => "Charlie", "age" => 30]
+];
+?>
